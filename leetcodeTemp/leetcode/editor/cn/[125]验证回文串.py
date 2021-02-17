e5c9c66259e25fbe2bfc6b635878941cdbce2e58
myhaa <32893249+myhaa@users.noreply.github.com>
2021-02-17 15:51:12 +0800

    fix:125,双指针

--- leetcodeTemp/leetcode/editor/cn/[125]验证回文串.py
+++ leetcodeTemp/leetcode/editor/cn/[125]验证回文串.py
@@ -18,17 +18,34 @@
 
 
 # leetcode submit region begin(Prohibit modification and deletion)
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         s = [x.lower() for x in s if (x.lower() >= 'a' and x.lower() <= 'z') or (x.lower() >= '0' and x.lower() <= '9')]
+#         if not s:
+#             return True
+#         mid = len(s) // 2
+#         left = s[:mid]
+#         right = s[mid:]
+#         tmp = min(len(left), len(right))
+#         if left[:tmp] == right[::-1][:tmp]:
+#             return True
+#         return False
+
+# 双指针
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s = [x.lower() for x in s if (x.lower() >= 'a' and x.lower() <= 'z') or (x.lower() >= '0' and x.lower() <= '9')]
-        if not s:
-            return True
-        mid = len(s) // 2
-        left = s[:mid]
-        right = s[mid:]
-        tmp = min(len(left), len(right))
-        if left[:tmp] == right[::-1][:tmp]:
-            return True
-        return False
+	def isPalindrome(self, s: str) -> bool:
+		s = [x.lower() for x in s if (x.lower() >= 'a' and x.lower() <= 'z') or (x.lower() >= '0' and x.lower() <= '9')]
+		if not s:
+			return True
+		left, right = 0, len(s)-1
+		while left < right:
+			# while left<right and ~((s[left].lower() >= 'a' and s[left].lower() <= 'z') or (s[left].lower() >= '0' and s[left].lower() <= '9')):
+			# 	left += 1
+			# while right>0 and ~((s[right].lower() >= 'a' and s[right].lower() <= 'z') or (s[right].lower() >= '0' and s[right].lower() <= '9')):
+			# 	right -= 1
+			if s[left] != s[right]:
+				return False
+			left, right = left+1, right-1
+		return True
 
 # leetcode submit region end(Prohibit modification and deletion)
