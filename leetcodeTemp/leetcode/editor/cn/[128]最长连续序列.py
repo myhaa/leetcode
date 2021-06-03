2c3486639883d74f56a734506ef8ae63f5e267e5
myhaa <32893249+myhaa@users.noreply.github.com>
2021-06-03 09:50:30 +0800

    dev: 128 最长连续序列
    并查集

--- leetcodeTemp/leetcode/editor/cn/[128]最长连续序列.py
+++ leetcodeTemp/leetcode/editor/cn/[128]最长连续序列.py
@@ -31,13 +31,16 @@
 #  Related Topics 并查集 数组
 #  👍 785 👎 0
 
+
+# leetcode submit region begin(Prohibit modification and deletion)
+
+# 排序
 # 思路：使用python自带排序算法排序，再使用动态规划判断最大连续数组长度
 # 时间复杂度：O(nlogn)
 # 空间复杂度：O(N)
 # 			执行耗时:84 ms,击败了23.05% 的Python3用户
 # 			内存消耗:22.6 MB,击败了13.47% 的Python3用户
 
-# leetcode submit region begin(Prohibit modification and deletion)
 # class Solution:
 #     def longestConsecutive(self, nums: List[int]) -> int:
 #         if len(nums) <= 1:
@@ -59,18 +62,78 @@
 # 空间复杂度：O(N)
 # 			执行耗时:68 ms,击败了29.77% 的Python3用户
 # 			内存消耗:26 MB,击败了7.11% 的Python3用户
+# class Solution:
+#     def longestConsecutive(self, nums: List[int]) -> int:
+#         ans = 0  # 最终答案
+#         nums_set = set(nums)  # 去重
+#         for i in nums_set:  # 遍历每个元素
+#             if i - 1 not in nums_set:  # 用哈希表判断是否存在，时间复杂度为O(1)
+#                 cur = 1  # 以i为起点的连续序列长度
+#                 x = i  # x + 1
+#
+#                 while x + 1 in nums_set:  # 一直累加判断是否在set
+#                     x += 1
+#                     cur += 1
+#                 ans = max(ans, cur)
+#         return ans
+
+
+# 并查集
+# 思路：将列表弄成并查集，并查集拥有合并，查找功能
+# 时间复杂度：O(N)
+# 空间复杂度：O(N)
+# 		执行耗时:304 ms,击败了15.05% 的Python3用户
+# 		内存消耗:29.5 MB,击败了5.04% 的Python3用户
+class UnionFind():
+    def __init__(self, arr):
+        self.parent = {}
+        for i in arr:
+            self.parent[i] = i  # 各自为战
+    
+    def union(self, p, q):
+        """
+        将p和q合并
+        :param p:
+        :param q:
+        :return:
+        """
+        root_p = self.find(p)
+        root_q = self.find(q)
+        if root_p == root_q:
+            return
+        if root_p is None or root_q is None:
+            return
+        if root_p < root_q:
+            self.parent[root_p] = root_q
+        else:
+            self.parent[root_q] = root_p
+    
+    def find(self, p):
+        """
+        找到p的头结点
+        :param p:
+        :return:
+        """
+        if p not in self.parent:
+            return
+        root_p = self.parent[p]
+        if root_p == p:
+            return p
+        else:
+            self.parent[p] = self.find(root_p)  # 路径压缩，每个结点的父节点都设为根节点
+            return self.parent[p]
+
+
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        ans = 0  # 最终答案
-        nums_set = set(nums)  # 去重
-        for i in nums_set:  # 遍历每个元素
-            if i - 1 not in nums_set:  # 用哈希表判断是否存在，时间复杂度为O(1)
-                cur = 1  # 以i为起点的连续序列长度
-                x = i  # x + 1
-                
-                while x + 1 in nums_set:  # 一直累加判断是否在set
-                    x += 1
-                    cur += 1
-                ans = max(ans, cur)
+        if len(nums) <= 1:
+            return len(nums)
+        
+        un = UnionFind(nums)  # init
+        for i in nums:
+            un.union(i, i+1)  # 将i和i+1合并
+        ans = 1
+        for i in nums:
+            ans = max(ans, un.find(i)-i+1)  # 找到最大路径
         return ans
 # leetcode submit region end(Prohibit modification and deletion)
