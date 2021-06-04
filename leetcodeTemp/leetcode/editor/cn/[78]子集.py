84534eb8d6e1c414e0a7e69fc18653dab941c34d
myhaa <32893249+myhaa@users.noreply.github.com>
2021-06-04 09:53:27 +0800

    dev: 78 子集
    回溯+数组迭代

--- leetcodeTemp/leetcode/editor/cn/[78]子集.py
+++ leetcodeTemp/leetcode/editor/cn/[78]子集.py
@@ -39,17 +39,53 @@
 # 空间复杂度： O(N)
 # 		执行耗时:36 ms,击败了85.76% 的Python3用户
 # 		内存消耗:14.7 MB,击败了95.77% 的Python3用户
+# class Solution:
+#     def f(self, nums, i, res):
+#         if i == len(nums):
+#             ans.append(res)
+#             return
+#         self.f(nums, i+1, res)
+#         self.f(nums, i+1, res+[nums[i]])
+#
+#     def subsets(self, nums: List[int]) -> List[List[int]]:
+#         global ans
+#         ans = []
+#         self.f(nums, 0, [])
+#         return ans
+
+# 回溯算法
+#		执行耗时:44 ms,击败了38.81% 的Python3用户
+#		内存消耗:15.1 MB,击败了23.49% 的Python3用户
+# class Solution:
+#     def subsets(self, nums: List[int]) -> List[List[int]]:
+#         def traceback(index):
+#             ans.append(res[:])
+#             for i in range(index, len(nums)):
+#                 res.append(nums[i])
+#                 traceback(i+1)
+#                 res.pop()
+#         ans = []
+#         res = []
+#         traceback(0)
+#         return ans
+
+# class Solution:
+#     def subsets(self, nums: List[int]) -> List[List[int]]:
+#         def traceback(index, tmp):
+#             ans.append(tmp)
+#             for i in range(index, len(nums)):
+#                 traceback(i+1, tmp+[nums[i]])
+#         ans = []
+#         traceback(0, [])
+#         return ans
+
+# 数组迭代
+#		执行耗时:36 ms,击败了85.76% 的Python3用户
+#		内存消耗:15 MB,击败了52.36% 的Python3用户
 class Solution:
-    def f(self, nums, i, res):
-        if i == len(nums):
-            ans.append(res)
-            return
-        self.f(nums, i+1, res)
-        self.f(nums, i+1, res+[nums[i]])
-        
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        global ans
-        ans = []
-        self.f(nums, 0, [])
-        return ans
+        res = [[]]
+        for i in nums:
+            res += [x+[i] for x in res]
+        return res
 # leetcode submit region end(Prohibit modification and deletion)
